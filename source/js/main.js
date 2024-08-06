abe4c41e48856dacaca76e92936ad8e35da05b07
Maxim Radionov <maxim_radionov@bk.ru>
2024-08-06 10:50:49 +0300

    fix: вносит правки

--- source/js/main.js
+++ source/js/main.js
@@ -4,14 +4,16 @@ import {initHeader} from './modules/header/init-header';
 import {initModals} from './modules/modal/init-modals';
 import {initPhoneMask} from './modules/init-phone-mask';
 import {initPhoneValidation} from './modules/init-phone-validation';
+import {initTabs} from './modules/tabs/init-tabs';
 
 
 window.addEventListener('DOMContentLoaded', () => {
+  initHeader();
+  initModals();
+  initAccordion();
+  initTabs();
   window.addEventListener('load', () => {
-    initHeader();
-    initModals();
     initCustomSelect();
-    initAccordion();
     initPhoneMask();
     initPhoneValidation();
   });
